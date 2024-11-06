b4851683748e923b37bc8959530860f3484a9edb
Sirawit-Boon <ter.sirawit1997@gmail.com>
2024-11-06 11:59:09 +0700

    algorithm exercise1

--- ex-3.js
+++ ex-3.js
@@ -1,5 +1,14 @@
 function singleNumber(nums) {
   // Start coding here
+  let result = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i]) {
+      result++
+    } else {
+      result = 1
+    }
+  }
+  return result
 }
 
 let result1 = singleNumber([2, 2, 1]);
